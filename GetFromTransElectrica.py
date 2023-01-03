278d6e504c06b8700ee2c01f99e8780664054048
ENDAVA\vpalasanu <vlad.palasanu@endava.com>
2023-01-03 15:23:48 +0200

    Initial version

--- GetFromTransElectrica.py
+++ GetFromTransElectrica.py
@@ -15,7 +15,7 @@ option.add_argument('--disable-gpu')
 option.add_argument('--disable-dev-sh-usage')
 
 URL = "https://www.transelectrica.ro/"
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
 
 driver.get('https://www.transelectrica.ro/')
 time.sleep(10)
