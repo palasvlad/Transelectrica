b96c2f7cef72c420cd5b8644828c8c603a969531
ENDAVA\vpalasanu <vlad.palasanu@endava.com>
2023-01-04 13:44:53 +0200

    linux branch

--- firefox.py
+++ firefox.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-browser = webdriver.Firefox(executable_path="./selenium-firefox/drivers/geckodriver")
+browser = webdriver.Firefox(executable_path="/app/delete/Transelectrica/selenium-firefox/drivers/geckodriver")
 browser.get('https://www.linuxhint.com')
 print('Title: %s' % browser.title)
 browser.quit()
