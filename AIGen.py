be133c50cd0b5b77bc6be9a9186fc05854516bee
ENDAVA\vpalasanu <vlad.palasanu@endava.com>
2023-01-04 16:02:28 +0200

    linux branch

--- AIGen.py
+++ AIGen.py
@@ -3,22 +3,9 @@ import time
 
 from selenium import webdriver
 
-# Create a Docker client
-client = docker.from_env()
-
-# Start a Selenium Docker container
-container = client.containers.run(
-    "selenium/standalone-chrome:latest",
-    detach=True,
-    ports={'4444/tcp': '4444'}
-)
-
-# Wait for the container to start
-time.sleep(10)
-
 # Connect to the Selenium server running in the container
 driver = webdriver.Remote(
-    command_executor='http://localhost:4444/wd/hub',
+    command_executor='http://192.168.0.188:4444/wd/hub',
     desired_capabilities=webdriver.DesiredCapabilities.CHROME
 )
 
