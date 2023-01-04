5e4b64a53f95968ea9a28b15a8ae558d49ff4920
ENDAVA\vpalasanu <vlad.palasanu@endava.com>
2023-01-04 17:13:28 +0200

    linux branch

--- AIGen.py
+++ AIGen.py
@@ -8,7 +8,7 @@ import pymysql
 # Connect to the Selenium server running in the container
 driver = webdriver.Remote(
     command_executor='http://192.168.0.188:4444/wd/hub',
-    desired_capabilities=webdriver.DesiredCapabilities.CHROME
+    options=webdriver.ChromeOptions()
 )
 
 # Navigate to a web page
